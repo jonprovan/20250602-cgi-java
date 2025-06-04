1b872ca55b9bdee6d81c6f6fe552ca5da0f56c40
Jon Provan <jprovan@skillstorm.com>
2025-06-04 11:24:38 -0400

    W1D3 Morning, Part 1

--- week-01/day-2/Examples/src/com/skillstorm/Operators.java
+++ week-01/day-2/Examples/src/com/skillstorm/Operators.java
@@ -63,7 +63,153 @@ public class Operators {
 		 * if (!(x == 5 && z == 11))
 		 * 
 		 */
+		
+		// Math operator examples
+		int myInt = 100;
+		System.out.println(myInt);
+		
+		myInt = myInt + 100;
+		System.out.println(myInt);
+		
+		myInt += 100;
+		System.out.println(myInt);
+		
+		myInt = 100;
+		
+		myInt %= 75;
+		System.out.println(myInt);
+		
+		// overflow/underflow
+		byte myByte = 127;
+		System.out.println(myByte);
+		
+		myByte += 1;
+		System.out.println(myByte);
+		
+		myByte -= 1;
+		System.out.println(myByte);
+		
+		/*
+		 * Computers think of values in binary
+		 * a byte is 8 bits, and a bit can be 0 or 1 ONLY!
+		 * so, a minimum-value byte is seen by a computer as 00000000
+		 * and a max-value byte is 11111111
+		 * 00000000, 00000001, 00000010, 00000011, 00000100
+		 * after max value, we'd be at 100000000 (a 1 and 8 zeroes)
+		 */
+		
+		// ORDER OF OPERATIONS
+		// PEMDAS applies -- Parentheses, Exponents, Mult, Div, Add, Sub
+		// * and / are at equal priority, also + and -
+		// those go left to right
+		int order = 1 + 6 / 2 * 10 - 5;
+		System.out.println(order);
+		
+		order = 1 + 3 * 10 - 5;
+		order = 1 + 30 - 5;
+		
+		// you can use parentheses to highlight the order or make it more readable
+		order = 1 + ((6 / 2) * 10) - 5;
+		
+		// you can also use it to change the order if you like
+		order = 1 + 6 / 2 * (10 - 5);
+		System.out.println(order);
+		
+		// the assignment operator (=) takes least precedence!
+		// we need to evaluate ALL the operations on the right before assigning the value to the thing on the left
+		
+		// you can operate on variable values, too!
+		int x = 65;
+		int y = 75;
+		int z = x + y;
+		
+		System.out.println(z);
+		
+		// watch out for your variable types when dividing!!
+		// this works fine, because 4 / 2 is an integer
+		int a = 4 / 2;
+		System.out.println(a);
+		
+		// this doesn't, because we're dividing integers, so we get an integer answer -- 2.5 gets truncated to 2
+		int b = 5 / 2;
+		System.out.println(b);
+		
+		// even if we change the variable type, the right-hand side still evaluates to an integer, because we're dividing integers
+		double c = 5 / 2;
+		System.out.println(c);
+		
+		// we can add decimals to one or both numbers on the right to have them divide as doubles
+		double d = 5.0 / 2.0;
+		System.out.println(d);
+		
+		// or we can cast one or both on the right to doubles
+		double e = (double)5 / 2;
+		System.out.println(e);
+		
+		// this will throw an Exception, specifically an ArithmeticException, so we can't do it
+		// int uhuh = 1/0;
+		
+		// doubles divided by 0 return Infinity, which is still kind of a problem, but no Exception
+		double nope = (double)1/0;
+		System.out.println(nope);
+		
+		// INCREMENTING/DECREMENTING
+		int value = 1;
+		System.out.println(value);
+		
+		value += 1;
+		System.out.println(value);
+		
+		// this does the same thing as the previous incrementation
+		value++;
+		System.out.println(value);
+		
+		// so does this
+		++value;
+		System.out.println(value);
+		
+		// however, if we're using the value in some way, not simply incrementing it, the placement of the ++ matters
+		// in this case -- a prefix increment -- we increment the value FIRST, then return the new value
+		System.out.println(++value);
+		
+		// here -- a postfix increment -- we return the CURRENT value, THEN increment it
+		System.out.println(value++);
+		
+		// we can see here that the value was in fact incremented in the last statement
+		System.out.println(value);
+		
+		int newValue = 10;
+		
+		// here, the equation takes the value first, then increments later
+		System.out.println(20 + newValue++);
+		
+		System.out.println(newValue);
+		
+		// here, the called method takes the value first, then increments later
+		System.out.println(returnValue(newValue++));
+		
+		int index = 0;
+		
+		// pulling the current value first can be useful in many situations
+		while (index < 5) {
+			System.out.println("Loop Index = " + index++);
+		}
+		
+		// works the same for decrementing
+		int otherIndex = 0;
+		
+		System.out.println(--otherIndex);
 
+		System.out.println(otherIndex--);
+		
+		System.out.println(otherIndex);
+		
+		
+
+	}
+	
+	public static int returnValue(int input) {
+		return input;
 	}
 
 }
---
+++ week-01/day-3/Day3Examples/src/com/skillstorm/MathClass.java
@@ -0,0 +1,101 @@
+// package declaration comes BEFORE imports
+package com.skillstorm;
+
+// imports come BEFORE class declaration
+// when you type an import into your code, press Ctrl-Space to show the list of possible imports
+// then press Enter on the one you want to import it
+import java.util.Random;
+
+public class MathClass {
+
+	public static void main(String[] args) {
+		
+		/*
+		 * Java has a Math class we can access for various mathematical methods and operations
+		 * These methods are static methods, meaning we don't need a Math object
+		 * We can just call the methods from the Math class directly
+		 * 
+		 * There are dozens of methods in this class, but a few are particularly useful
+		 * 
+		 * .pow() -- for using exponents
+		 * .sqrt() -- for getting the square root
+		 * .floor() -- for getting the next smallest integer below a double value -- rounding down!
+		 * .ceil() -- rounding up
+		 * .random() -- generating a random number between 0 and 1
+		 */
+		
+		// .pow()
+		// the first parameter is the number you want to take to the second parameter power, so this is 2 to the 8th power
+		// NOTE -- static methods and properties show up in italics!
+		double exponentResult = Math.pow(2, 8);
+		
+		// NOTE -- final properties and methods show up in bold
+		System.out.println(exponentResult);
+		
+		// negative powers work, too
+		exponentResult = Math.pow(10, -3);
+		
+		System.out.println(exponentResult);
+		
+		// PEMDAS holds up here -- the pow() operation has to resolve before multiplying by 9
+		double result = 3 + 9 * Math.pow(2, 2);
+		
+		System.out.println(result);
+		
+		
+		// sqrt()
+		// returns the square root of the single parameter
+		double root = Math.sqrt(81);
+		
+		System.out.println(root);
+		
+		// in math-nerd terms, this SHOULD be 9i...
+		root = Math.sqrt(-81);
+		
+		// ...but Java doesn't understand imaginary numbers...alas!
+		System.out.println(root);
+		
+		
+		// .floor() and .ceil() work similarly, rounding up or down to the next integer, respectively
+		double floor = Math.floor(105.35);
+		
+		System.out.println(floor);
+		
+		double ceiling = Math.ceil(105.35);
+		
+		System.out.println(ceiling);
+		
+		// can use these to round to certain decimal point limits
+		double price = 599.34567896578;
+		
+//		price *= 100;
+//		price = Math.floor(price);
+//		price /= 100;
+		
+		price = Math.floor(price * 100) / 100;
+		
+		System.out.println(price);
+		
+		
+		// .random()
+		double random = Math.random();
+		
+		System.out.println(random);
+		
+		int randomOneTen = (int)Math.ceil(Math.random() * 10);
+		
+		// same result, but Elizabeth's ceiling method is better and more fun!
+		// randomOneTen = (int)(Math.random() * 10 + 1);
+		
+		System.out.println(randomOneTen);
+		
+		// using the Random class, there's a shorthand
+		// note that this involves instantiating a new Object from the Random class, then calling one of its methods
+		int randomInt = new Random().nextInt(10) + 1;
+		System.out.println(randomInt);
+		
+		
+
+	}
+
+}
