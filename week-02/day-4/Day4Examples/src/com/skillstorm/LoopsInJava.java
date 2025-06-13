c298db0b5a7983817f25f6111590d91e2032bbed
Jon Provan <jprovan@skillstorm.com>
2025-06-13 10:16:26 -0400

    W2D5 Morning, Part 1

--- week-02/day-4/Day4Examples/src/com/skillstorm/LoopsInJava.java
+++ week-02/day-4/Day4Examples/src/com/skillstorm/LoopsInJava.java
@@ -1,6 +1,8 @@
 package com.skillstorm;
 
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Scanner;
 
 public class LoopsInJava {
@@ -59,33 +61,116 @@ public class LoopsInJava {
 		System.out.println("Do-While Loop COMPLETE!");
 		
 		// with user input
-		Scanner scan = new Scanner(System.in);
+//		Scanner scan = new Scanner(System.in);
+//		
+//		System.out.println("\nPlease Enter Your Name:");
+//		
+//		String[] names = { "Elizabeth", "Jeff", "Jon", "Michael", "Tiffiny" };
+//		
+//		boolean redo = false;
+//		String result = "";
+//		
+//		do {
+//			String input = scan.nextLine().trim();
+//			if (Arrays.binarySearch(names, input) < 0) {
+//				redo = true;
+//				System.out.println("Please try again:");
+//			} else {
+//				redo = false;
+//				result = input;
+//			}
+//		} while (redo);
+//		
+//		System.out.println("Your name is: " + result);
+//		
+//		scan.close();
 		
-		System.out.println("\nPlease Enter Your Name:");
 		
-		String[] names = { "Elizabeth", "Jeff", "Jon", "Michael", "Tiffiny" };
+		// the standard for loop
+		// usually, this loop will create an internal index, increment it until some value, then stop and remove the variable from memory
+		// there are three things that go into the setup of a standard for loop
+		// 1. something to do ONCE BEFORE the loop EVER starts (usually setting up the variable)
+		// 2. a condition to check BEFORE EACH iteration of the loop
+		// 3. something to do AFTER each iteration of the loop (usually incrementing the variable)
 		
-		boolean redo = false;
-		String result = "";
+		/*
+		 *  SYNTAX
+		 *  
+		 *  for (<something to do once before>; <condition>; <something to do after each>) {
+		 *  ...some code
+		 *  }
+		 */
 		
-		do {
-			String input = scan.nextLine().trim();
-			if (Arrays.binarySearch(names, input) < 0) {
-				redo = true;
-				System.out.println("Please try again:");
-			} else {
-				redo = false;
-				result = input;
-			}
-		} while (redo);
+		System.out.println();
+
+		for (int i = 0; i < 5; i++) {
+			System.out.println("Standard For Loop Index Currently at: " + i);
+		}
 		
-		System.out.println("Your name is: " + result);
+		System.out.println();
 		
-		scan.close();
+		// since the index starts at zero (usually) and increments, this structure is GREAT for array/collection access
+		// the enhanced for DOES NOT have an index, so you have do some extra work if you want index access there
+		// whether or not you do anything with each value in the array is up to you
+		// you can use conditionals to choose whether or not to process each one, etc.
+		
+		String[] movies = { "The Boondock Saints", "The Hobbit Trilogy", "Dead Alive", "28 Years Later", "Fried Green Tomatoes" };
+		
+		for (int i = 0; i < movies.length; i++) {
+			System.out.println(movies[i]);
+		}
+		
+		System.out.println();
 
 		
+		// the enhanced for loop
+		// for each item in an array or collection or anything iterable...
+		// do something!
+		// there's no index being incremented here, so one disadvantage is we don't have index-based access
+		// HOWEVER, we define a variable on setup that automatically has the value of whichever item we're looking at
 		
+		for (String movie : movies) {
+			System.out.println(movie);
+		}
 		
+		System.out.println();
+		
+		int[][] nums = { { 1, 2, 3 }, { 4, 5, 6 }, { 7, 8, 9 } };
+		
+		for (int[] line : nums) {
+			System.out.println(Arrays.toString(line));
+		}
+		
+		System.out.println();
+		
+		// with Objects...
+		Film[] films = { new Film("Barry Lyndon"), new Film("The Joy Luck Club"), new Film("Do The Right Thing") };
+		
+		for (Film film : films) {
+			System.out.println(film.title);
+		}
+		
+		System.out.println();
+		
+		// some collections can't be iterated, so they don't work with enhanced for
+		Map<Integer, String> students = new HashMap<>();
+		
+		students.put(6, "Tiffiny");
+		students.put(2, "Jeff");
+		students.put(3, "Elizabeth");
+		students.put(5, "Michael");
+		
+		System.out.println(students.get(3));
+		
+		// can't do this, because Maps are not iterable!
+//		for (Integer id : students) {
+//			System.out.println(student);
+//		}
+		
+		// we have to find something iterable to use
+		for (Integer id : students.keySet()) {
+			System.out.println(students.get(id));
+		}
 		
 		
 		
@@ -105,4 +190,12 @@ public class LoopsInJava {
 
 	}
 
+}
+
+class Film {
+	public String title;
+	
+	public Film(String title) {
+		this.title = title;
+	}
 }
