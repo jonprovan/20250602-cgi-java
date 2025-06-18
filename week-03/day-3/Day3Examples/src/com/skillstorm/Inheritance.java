e5168da389a31fed94deb54701de8b91c56390f8
Jon Provan <jprovan@skillstorm.com>
2025-06-18 11:19:52 -0400

    W3D3 Morning, Part 2

--- week-03/day-3/Day3Examples/src/com/skillstorm/Inheritance.java
+++ week-03/day-3/Day3Examples/src/com/skillstorm/Inheritance.java
@@ -29,6 +29,38 @@ public class Inheritance {
 		
 		System.out.println(person);
 		System.out.println(otherPerson);
+		
+		person.announceSelf();
+		
+		Employee employee1 = new Employee("Francine Poulet", 35, "Female", otherPerson, 12345, "Animal Control Officer");
+		
+		System.out.println(employee1);
+		
+		System.out.println(employee1.getAge());
+		
+		// this method is overridden
+		// when called on a Person object, it uses the version written there
+		// however, when called on an Employee object, it uses the override in the Employee class
+		otherPerson.announceSelf();
+		employee1.announceSelf();
+		
+		// regardless of how the compiler "views" an object/variable
+		// the version of an overridden method that runs is based on the actual object type
+		// this is runtime polymorphism at work -- until the object actually shows up and runs, you won't know which one gets selected
+		Person testEmployee = employee1;
+		testEmployee.announceSelf();
+		
+		String[] languages = { "Java", "JavaScript", "COBOL" };
+		SoftwareEngineer se1 = new SoftwareEngineer("Millard Fillmore", 77, "Male", null, 8675309, "Full-Stack Developer", languages);
+		
+		se1.announceSelf();
+		System.out.println(se1.getEmployeeId());
+		System.out.println(se1.getAge());
+		System.out.println(se1);
+		
+		Employee a = se1;
+		Person b = se1;
+		Object c = se1;
 
 	}
 
@@ -46,6 +78,7 @@ class Person {
 	private Person bestFriend;
 	
 	public Person(String name, int age, String gender, Person bestFriend) {
+		super();
 		this.name = name;
 		this.age = age;
 		this.gender = gender;
@@ -83,6 +116,11 @@ class Person {
 	public void setBestFriend(Person bestFriend) {
 		this.bestFriend = bestFriend;
 	}
+	
+	// a method we'll override later
+	public void announceSelf() {
+		System.out.println("My name is " + this.name + ".");
+	}
 
 	// this is an override of Object's toString() method
 	// without an override, when we print our object, it'll just print the class name plus a hashCode
@@ -97,3 +135,101 @@ class Person {
 	}
 	
 }
+
+// this class inherits from Person, which of course inherits from Object
+// we use the "extends" keyword to indicate which class it inherits DIRECTLY from
+class Employee extends Person {
+	
+	// properties that Employee has but Person DOES NOT
+	private int employeeId;
+	private String jobTitle;
+	
+	public Employee(String name, int age, String gender, Person bestFriend, int employeeId, String jobTitle) {
+		
+		// we already wrote the constructor functionality for these fields
+		// so we can just use the parent class's constructor
+		// this is called "constructor chaining"
+		super(name, age, gender, bestFriend);
+		this.employeeId = employeeId;
+		this.jobTitle = jobTitle;
+	}
+
+	public int getEmployeeId() {
+		return employeeId;
+	}
+
+	public void setEmployeeId(int employeeId) {
+		this.employeeId = employeeId;
+	}
+
+	public String getJobTitle() {
+		return jobTitle;
+	}
+
+	public void setJobTitle(String jobTitle) {
+		this.jobTitle = jobTitle;
+	}
+	
+	@Override
+	public void announceSelf() {
+		System.out.println("My name is " + this.getName() + ", and my job title is " + jobTitle + ".");
+	}
+
+	@Override
+	public String toString() {
+		return "Employee [name=" + getName() + ", age=" + getAge() + ", gender=" + getGender() + 
+				", bestFriend=" + ((getBestFriend() != null) ? getBestFriend().getName() : getBestFriend()) + 
+				", employeeId=" + employeeId + ", jobTitle=" + jobTitle + "]";
+	}
+	
+}
+
+// this class inherits from Employee, which inherits from Person, which inherits from Objects
+class SoftwareEngineer extends Employee {
+	
+	private String[] languages;
+	
+	public SoftwareEngineer(String name, int age, String gender, Person bestFriend, int employeeId, String jobTitle, String[] languages) {
+		
+		// each class ONLY has access to whichever constructors are available in the parent for the super call
+		// in our case, the ONLY constructor in Employee takes these six arguments
+		super(name, age, gender, bestFriend, employeeId, jobTitle);
+		this.languages = languages;
+	}
+
+	public String[] getLanguages() {
+		return languages;
+	}
+
+	public void setLanguages(String[] languages) {
+		this.languages = languages;
+	}
+	
+	@Override
+	public void announceSelf() {
+		System.out.println("My name is " + this.getName() + 
+						   ", and my job title is " + this.getJobTitle() + 
+						   ", and I'm proficient in " + Arrays.toString(this.languages) + ".");
+	}
+	
+	@Override
+	public String toString() {
+		return "Employee: Languages = " + Arrays.toString(this.languages);
+	}
+	
+	
+	
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
