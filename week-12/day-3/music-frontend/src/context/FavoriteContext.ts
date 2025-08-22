26a801f8cf8323f71c7e1f151edb07464554ffbf
Jon Provan <jprovan@skillstorm.com>
2025-08-22 15:08:22 -0400

    W12D5 Afternoon Context Fix

--- week-12/day-3/music-frontend/src/context/FavoriteContext.ts
+++ week-12/day-3/music-frontend/src/context/FavoriteContext.ts
@@ -1,5 +1,9 @@
 import { createContext } from "react";
 
 // if you put a default value in the parentheses, it'll type the context to that type
-// if you leave it blank, it'll be able to hold anything!
-export const FavoriteContext = createContext();
+// if you leave it blank and leave the createContext method untyped, it'll be able to hold anything!
+// like this -- export const FavoriteContext = createContext();
+// here, we're typing the function AND giving it a default value
+// BE AWARE -- by typing it this way, 
+// we'll have to come back to this file if we add things to the context in Frame.tsx
+export const FavoriteContext = createContext<[string, (favorite: string) => void]>(['', () => {}]);
