918fe8cc36f476dd1d8102b7c7f4ad0b0964b44c
Jon Provan <jprovan@skillstorm.com>
2025-09-03 11:53:40 -0400

    W14D3 Morning Final

--- week-14/day-3/eureka-server/src/main/java/com/skillstorm/EurekaServerApplication.java
+++ week-14/day-3/eureka-server/src/main/java/com/skillstorm/EurekaServerApplication.java
@@ -2,8 +2,12 @@ package com.skillstorm;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+// this annotation is the ONLY thing we need code-wise to make this work as a Eureka server
+// this application will now come up and be available for other services to register themselves for service discovery
+@EnableEurekaServer
 public class EurekaServerApplication {
 
 	public static void main(String[] args) {
